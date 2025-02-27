418db98b185fd55d22ec14405fa9a4ac2deaa40b
AmaseCocoa <cocoa@amase.cc>
2025-02-27 21:53:57 +0900

    chore: test server

--- test-server.py
+++ test-server.py
@@ -1,29 +1,61 @@
-from apmodel import Create, Multikey, Person
-from apmodel.vocab.object import Note
+import asyncio
+import contextlib
+import json
+import random
+import string
+import sys
+import uuid
+
+import aiohttp
+import uvicorn
+from apmodel import Accept, Create, Multikey, Person
 from apmodel.nodeinfo.ni21.nodeinfo import NodeInfo, Software
 from apmodel.schema.propertyvalue import PropertyValue
 from apmodel.security.cryptographickey import CryptographicKey
-from apsig import KeyUtil
+from apmodel.vocab.object import Note
+from apsig import KeyUtil, draftSigner
+from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import ed25519, rsa
 from starlette.applications import Starlette
-from starlette.middleware import Middleware
 from starlette.requests import Request
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, Response
+from taskiq import InMemoryBroker
 
 from apkit import APKit
-from apkit.x.starlette import ActivityPubMiddleware
 
 ap = APKit()
-priv = rsa.generate_private_key(key_size=2048, public_exponent=65537)
+broker = InMemoryBroker()
+
+priv = rsa.generate_private_key(
+    key_size=3072, public_exponent=65537, backend=default_backend()
+)
 ed_privatekey = ed25519.Ed25519PrivateKey.generate()
+
+
+def generate_random_string(length):
+    # 使用する文字のセット（英大文字、英小文字、数字）
+    characters = string.ascii_letters + string.digits
+    # ランダムな文字列を生成
+    random_string = "".join(random.choice(characters) for _ in range(length))
+    return random_string
+
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+random_str = generate_random_string(8)
+actorId = str(uuid.uuid4())
+noteId = str(uuid.uuid4())
+print(actorId)
+print(noteId)
 keyutl = KeyUtil(private_key=ed_privatekey)
 act = Person(
-    id="https://test2.amase.cc/actor",
-    preferredUsername="test",
+    id=f"https://test2.amase.cc/users/{actorId}",
+    preferredUsername=random_str,
     publicKey=CryptographicKey(
-        id="https://test2.amase.cc/actor#main-key",
-        owner="https://test2.amase.cc/actor",
+        id=f"https://test2.amase.cc/users/{actorId}#main-key",
+        owner=f"https://test2.amase.cc/users/{actorId}",
         publicKeyPem=priv.public_key()
         .public_bytes(
             serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo
@@ -32,8 +64,8 @@ act = Person(
     ),  # type: ignore
     assertionMethod=[
         Multikey(
-            id="https://test2.amase.cc/actor#ed25519-key",
-            controller="https://test2.amase.cc/actor",
+            id=f"https://test2.amase.cc/users/{actorId}#ed25519-key",
+            controller=f"https://test2.amase.cc/users/{actorId}",
             publicKeyMultibase=keyutl.encode_multibase(),
         )
     ],
@@ -46,16 +78,45 @@ act = Person(
     ],
     inbox="https://test2.amase.cc/inbox",
     sharedInbox="https://test2.amase.cc/inbox",
+    to=["https://www.w3.org/ns/activitystreams#Public"],
 )
 
 note = Note(
-    id='https://test2.amase.cc/notes/1', 
-    attributedTo="https://test2.amase.cc/actor",
-    content='<p>投稿内容</p><br><a href="https://example.com">example.com</a>',
-    to=[
-        'https://www.w3.org/ns/activitystreams#Public'
-    ]
+    id=f"https://test2.amase.cc/notes/{noteId}",
+    attributedTo=f"https://test2.amase.cc/users/{actorId}",
+    content='<p>Testing APKit Inbox Post</p><br><a href="https://example.com">example.com</a>',
+    to=["https://www.w3.org/ns/activitystreams#Public"],
 )
+created = Create(
+    id=f"https://test2.amase.cc/notes/{noteId}/activity", object=note, actor=act.id
+)
+print(act.id)
+post_base = "https://test1.amase.cc"
+post_base = "https://mstdn.amase.cc"
+post_base = "http://localhost:61685"
+
+@broker.task
+async def post_inbox() -> None:
+    signer = draftSigner()
+    created_dict = created.to_dict()
+    headers = signer.sign(
+        priv,
+        "POST",
+        f"{post_base}/inbox",  # https://test1.amase.cc
+        headers={"Content-Type": "application/activity+json"},
+        key_id=f"https://test2.amase.cc/users/{actorId}#main-key",
+        body=json.dumps(created_dict, ensure_ascii=False).encode("utf-8"),
+    )
+    async with aiohttp.ClientSession(headers=headers) as session:
+        print(headers)
+        print("---")
+        print(created_dict)
+        print("---")
+        async with session.post(
+            f"{post_base}/inbox", json=created_dict
+        ) as resp:  # https://test1.amase.cc
+            print(await resp.text())
+            print(resp.status)
 
 
 @ap.nodeinfo("2.1")
@@ -71,38 +132,84 @@ async def nodeinfo():
     )
 
 
+@ap.on_accept()
+async def accept(request: Request, accept: Accept):
+    print(json.dumps(accept.to_dict(), ensure_ascii=False, indent=4))
+    return Response(status_code=201)
+
+
 @ap.on_create()
-async def inbox(request: Request, activity: Create):
+async def create(request: Request, activity: Create):
     pass
 
 
-app = Starlette(middleware=[Middleware(ActivityPubMiddleware, apkit=ap)])
+@contextlib.asynccontextmanager
+async def lifespan(app):
+    await broker.startup()
+    yield
+    await broker.shutdown()
 
-@app.route("/notes/1", methods=["GET"])
+
+app = Starlette(
+    middleware=[],
+    lifespan=lifespan,  # Middleware(ActivityPubMiddleware, apkit=ap)
+)
+
+
+@app.route("/api/notes/create", methods=["POST"])
+async def create_post(request: Request):
+    if request.headers.get("token") != "ebxBE7btjL":
+        return Response(status_code=403)
+    else:
+        await post_inbox.kiq()
+        return JSONResponse(content=note.to_dict(), media_type="application/json")
+
+
+@app.route(f"/notes/{noteId}/activity", methods=["GET"])
+async def notes_activity(request: Request):
+    return JSONResponse(
+        content=created.to_dict(), media_type="application/activity+json"
+    )
+
+
+@app.route(f"/notes/{noteId}", methods=["GET"])
 async def notes_1(request: Request):
     return JSONResponse(content=note.to_dict(), media_type="application/activity+json")
 
-@app.route("/actor", methods=["GET"])
-async def actor(request: Request):
+
+@app.route(f"/users/{actorId}", methods=["GET"])
+async def user(request: Request):
     return JSONResponse(content=act.to_dict(), media_type="application/activity+json")
+
+
+@app.route("/inbox", methods=["POST"])
+async def nibox(request: Request):
+    print(request.headers)
+    print("---")
+    print(await request.json())
+    print("---")
+    return Response(status_code=503)
+
+
+@app.route("/", methods=["GET"])
+async def idx(request: Request):
+    return Response("Hi")
 
 
 @app.route("/.well-known/webfinger", methods=["GET"])
 async def webfinger(request: Request):
     return JSONResponse(
         content={
-            "subject": "acct:test@test2.amase.cc",
+            "subject": f"acct:{random_str}@test2.amase.cc",
             "links": [
                 {
                     "rel": "self",
                     "type": "application/activity+json",
-                    "href": "https://test2.amase.cc/actor",
+                    "href": f"https://test2.amase.cc/users/{actorId}",
                 },
             ],
         }
     )
 
-
-import uvicorn
 
 uvicorn.run(app, port=3002)
