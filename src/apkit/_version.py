930460861c895325f98c9931b5f54cffbe83472f
AmaseCocoa <cocoa@amase.cc>
2025-09-15 02:52:59 +0900

    feat: docs

--- src/apkit/_version.py
+++ src/apkit/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.2.0.post1.dev2+gf17c7e785.d20250912'
-__version_tuple__ = version_tuple = (0, 2, 0, 'post1', 'dev2', 'gf17c7e785.d20250912')
+__version__ = version = '0.3.0.post1.dev1+ga5f827980.d20250912'
+__version_tuple__ = version_tuple = (0, 3, 0, 'post1', 'dev1', 'ga5f827980.d20250912')
 
 __commit_id__ = commit_id = None
