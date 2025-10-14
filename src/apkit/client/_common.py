0ba0c618800bd595ff5a18d779084058119733f8
AmaseCocoa <cocoa@amase.cc>
2025-10-15 01:15:03 +0900

    feat(client): Add _is_expected_content_type to check expected content_type

--- src/apkit/client/_common.py
+++ src/apkit/client/_common.py
@@ -12,3 +12,16 @@ def validate_webfinger_result(result: WebfingerResult, expected_subject: Resourc
         raise ValueError(
             f"Mismatched subject in response. Expected {expected_subject}, got {result.subject}"
         )
+
+def _is_expected_content_type(actual_ctype: str, expected_ctype_prefix: str) -> bool:    
+    mime_type = actual_ctype.split(';')[0].strip().lower()
+
+    if mime_type == 'application/json':
+        return True
+    if mime_type.endswith('+json'):
+        return True
+    
+    if expected_ctype_prefix and mime_type.startswith(expected_ctype_prefix.split(';')[0].lower()):
+         return True
+
+    return False
