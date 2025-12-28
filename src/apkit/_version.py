90fae655ff960ea9356d78167f56a4465b8d44d6
AmaseCocoa <cocoa@amase.cc>
2025-12-29 06:25:48 +0900

    chore: update apsig to 0.6.0

--- src/apkit/_version.py
+++ src/apkit/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.3.3.post1.dev27+ge47c822fb.d20251228'
-__version_tuple__ = version_tuple = (0, 3, 3, 'post1', 'dev27', 'ge47c822fb.d20251228')
+__version__ = version = '0.3.3.post1.dev32+g5c3dfc179.d20251228'
+__version_tuple__ = version_tuple = (0, 3, 3, 'post1', 'dev32', 'g5c3dfc179.d20251228')
 
 __commit_id__ = commit_id = None
